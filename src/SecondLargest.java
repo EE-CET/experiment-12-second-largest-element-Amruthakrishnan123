07cacafa0460793966e569bd2079c146be94c5c4
Amruthakrishnan123 <amruthakrishnan936@gmail.com>
2026-04-05 12:30:46 +0530

    exp12--done

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -1,35 +1,36 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class SecondLargest {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        // Initialize variables
-        int first = Integer.MIN_VALUE;
-        int second = Integer.MIN_VALUE;
+    public static int findSecondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
 
         for (int num : arr) {
-            if (num > first) {
-                second = first;
-                first = num;
-            } else if (num > second && num != first) {
-                second = num;
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } 
+            else if (num > secondLargest && num != largest) {
+                secondLargest = num;
             }
         }
 
-        if (second == Integer.MIN_VALUE) {
-            System.out.println("No second largest element found.");
-        } else {
-            System.out.println(second);
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        
+
+        System.out.println(findSecondLargest(arr));
+
         sc.close();
     }
 }
